39628dad949b7d80167ea047694e89a284fa0ae1
Vivek Kumar <vivek.k@omaemirates.com>
2025-11-24 18:18:16 +0400

    Program changes

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ if (app.Environment.IsDevelopment())
 
 var summaries = new[]
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching","Scorching"
 };
 
 app.MapGet("/weatherforecast", () =>
